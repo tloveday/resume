35cbbd906e1a9d70f0807a971a3fcfbe1c234373
tloveday <thomas_loveday848@hotmail.com>
2021-05-27 13:20:59 +0000

    Implement emailJS on contact page

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -39,7 +39,7 @@ function repoInformationHTML(repos){
 function fetchGitHubInformation(event){
     $("#gh-user-data").html(" ");
     $("#gh-repo-data").html(" ");
-    
+
     var username = $("#gh-username").val();
     if (!username) {
         $("gh-user-data").html(`<h2>Please Enter A GitHub Username</h2>`);
@@ -64,9 +64,14 @@ function fetchGitHubInformation(event){
             if (errorResponse.status === 404){
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`)
+            }else if (errorResponse.status === 403){
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                $("gh-user-data").html(`<h4>Too Many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`)
             }else{
                 console.log(errorResponse);
                 $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`)
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
