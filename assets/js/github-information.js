33e31017270db4f768d86e04e284421c17afd094
tloveday <thomas_loveday848@hotmail.com>
2021-05-27 11:54:32 +0000

    Fix bugs for emtpty divs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,7 +37,9 @@ function repoInformationHTML(repos){
 }
 
 function fetchGitHubInformation(event){
-
+    $("#gh-user-data").html(" ");
+    $("#gh-repo-data").html(" ");
+    
     var username = $("#gh-username").val();
     if (!username) {
         $("gh-user-data").html(`<h2>Please Enter A GitHub Username</h2>`);
